dde4d3fb53207b34b11c3be4632d8297419fce5a
petersen <pmphareng@gmail.com>
2018-12-10 05:17:57 +0200

    changes analytics parameters

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,20 +12,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-130695869-1",
-        // Puts tracking script in the head instead of the body
         head: true,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
       },
     },
     'gatsby-plugin-react-helmet',
